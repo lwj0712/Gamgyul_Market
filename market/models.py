a42647c8f681e392ba3635f816b559e6d395d19b
Seong-jae.Park <bvc3435@gamil.com>
2024-09-25 09:16:58 +0900

    ✨ feat : market migrate

--- market/models.py
+++ market/models.py
@@ -44,7 +44,7 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Review by {self.user} for {self.product.name}"
+        return f"Review for {self.product.name}"
 
 
 # 영수증? 장부?
