663941db6761ca4692ae9158a4c48556e61ecd9b
Donggyu-Kim <kimdg000411@gmail.com>
2024-09-26 04:09:28 +0900

    :sparkles: feat: 소셜 로그인 기능 추가 - 구글 로그인 뷰 작성 - OAuth2 인증 시작(통신) 및 처리 기능 - 로그인 url 반환 뷰 작성 - 구글 로그인 페이지로 리다이렉트 기능 - 인증 과정 후 콜백 url 뷰 작성

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
-from django.contrib.auth import login, logout, authenticate
-from rest_framework import generics, status
+from rest_framework import status, generics
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.contrib.auth import get_user_model
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
+from django.conf import settings
+from django.contrib.auth import login, get_user_model, logout, authenticate
 from .serializers import UserSerializer, LoginSerializer, ProfileSerializer
 
 User = get_user_model()
@@ -52,6 +54,56 @@ class LoginView(APIView):
                     status=status.HTTP_400_BAD_REQUEST,
                 )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GoogleLoginView(SocialLoginView):
+    """
+    google 로그인 담당 처리 view
+    settings에 callbacks url 설정
+    """
+
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = settings.GOOGLE_CALLBACK_URI
+    client_class = OAuth2Client
+
+
+class GoogleLoginURLView(APIView):
+    """
+    Google login URL API view
+    """
+
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        adapter = GoogleOAuth2Adapter(request)
+        provider = adapter.get_provider()
+        app = provider.get_app(request)
+        client = OAuth2Client(
+            request,
+            app.client_id,
+            app.secret,
+            adapter.access_token_method,
+            adapter.access_token_url,
+            callback_url=settings.GOOGLE_CALLBACK_URI,
+        )
+        authorize_url = client.get_redirect_url()
+        return Response({"authorization_url": authorize_url}, status=status.HTTP_200_OK)
+
+
+class GoogleCallbackView(APIView):
+    """
+    Google callback view
+    """
+
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        code = request.GET.get("code", None)
+        if code:
+            return Response({"code": code}, status=status.HTTP_200_OK)
+        return Response(
+            {"error": "Code not found."}, status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 class LogoutView(APIView):
