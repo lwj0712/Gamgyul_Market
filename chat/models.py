fe94f1019b33f58d6de1b862f0e4daa54d43cace
lwj0712 <dldnjswot@naver.com>
2024-09-25 16:57:10 +0900

    🔧 conf: chat>models.py 업데이트
    - ChatRoom모델에 name field 추가

--- chat/models.py
+++ chat/models.py
@@ -4,10 +4,11 @@ from django.contrib.auth.models import User
 
 
 class ChatRoom(models.Model):
+    name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Room {self.id}"
+        return self.name
 
 
 class ChatParticipant(models.Model):
