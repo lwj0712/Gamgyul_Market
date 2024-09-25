16ecdc7b4e968fd89ab31daa3f001e0c9793e076
Donggyu-Kim <kimdg000411@gmail.com>
2024-09-25 21:08:27 +0900

    :wrench: conf: serializers.py 수정 - 회원가입 절차 추가 - 로그인 serializer - follow 정보, post 정보, product 정보 - post 모델 작성 후 필드명 수정 필요

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,41 +1,93 @@
 from rest_framework import serializers
-from .models import User, SocialAccount, Follow
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from insta.models import Comment, Post
+from market.models import Product
+
+User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    - 회원가입 serializer
+    - password2: 비밀번호 확인 메서드 후 회원 정보에서 제거
+    """
+
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = (
-            "id",
-            "username",
-            "email",
-            "profile_image",
-            "temperature",
-            "nickname",
-            "bio",
-            "latitude",
-            "longitude",
-        )
-        read_only_fields = ("id", "temperature")
+        fields = "__all__"
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "email": {"required": True},
+            "nickname": {"required": True},
+            "bio": {"required": False},
+            "profile_image": {"required": False},
+            "latitude": {"required": False},
+            "longitude": {"required": False},
+            "temperature": {"read_only": True},
+        }
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "비밀번호가 일치하지 않습니다."}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("password2")
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
-class SocialAccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SocialAccount
-        fields = ("id", "user", "provider", "uid")
-        read_only_fields = ("id",)
+class LoginSerializer(serializers.Serializer):
+    """
+    - 로그인 serializer
+    """
+
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
 
 
 class FollowSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Follow
-        fields = ("id", "follower", "following", "created_at")
-        read_only_fields = ("id", "created_at")
+        model = User
+        fields = ("id", "username", "nickname", "profile_image")
 
 
-class UserProfileSerializer(serializers.ModelSerializer):
+class CommentedPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ("id", "content", "created_at")
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    """
+    Product 정보 불러오기
+    """
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """
+    - 프로필 정보 표시
+    - 기본 정보, 마켓 정보, 팔로우 관계와 수, 댓글 단 post 내용
+    """
+
+    followers = serializers.SerializerMethodField()
+    following = serializers.SerializerMethodField()
     followers_count = serializers.SerializerMethodField()
     following_count = serializers.SerializerMethodField()
+    commented_posts = serializers.SerializerMethodField()
+    products = serializers.SerializerMethodField()
 
     class Meta:
         model = User
@@ -43,19 +95,37 @@ class UserProfileSerializer(serializers.ModelSerializer):
             "id",
             "username",
             "email",
-            "profile_image",
-            "temperature",
             "nickname",
             "bio",
-            "latitude",
-            "longitude",
+            "profile_image",
+            "temperature",
+            "followers",
+            "following",
             "followers_count",
             "following_count",
+            "commented_posts",
+            "products",
         )
-        read_only_fields = ("id", "temperature", "followers_count", "following_count")
+
+    def get_followers(self, obj):
+        return FollowSerializer(obj.followers.all(), many=True).data
+
+    def get_following(self, obj):
+        return FollowSerializer(obj.following.all(), many=True).data
 
     def get_followers_count(self, obj):
         return obj.followers.count()
 
     def get_following_count(self, obj):
         return obj.following.count()
+
+    def get_commented_posts(self, obj):
+        comments = (
+            Comment.objects.filter(user=obj).values_list("post", flat=True).distinct()
+        )
+        posts = Post.objects.filter(id__in=comments)
+        return CommentedPostSerializer(posts, many=True).data
+
+    def get_products(self, obj):
+        products = Product.objects.filter(user=obj)
+        return ProductSerializer(products, many=True).data
