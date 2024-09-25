b493d79a0548ff4a69de794641e31643d3eb132b
lwj0712 <dldnjswot@naver.com>
2024-09-25 09:22:31 +0900

    ✨ feat: chat>views.py 추가
    - 채팅방, 채팅방 생성 페이지 view

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def room(request, room_name):
+    return render(request, "chat/room.html", {"room_name": room_name})
+
+
+def create_room(request):
+    if request.method == "POST":
+        room_name = request.POST.get("room_name")
+        return redirect("room", room_name=room_name)
+    return render(request, "chat/create_room.html")
