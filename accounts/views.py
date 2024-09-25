3ead8d4be0bf750cc4bcf9cff19aa4588b0a9cac
Donggyu-Kim <kimdg000411@gmail.com>
2024-09-25 21:42:20 +0900

    :wrench: conf: accounts/views.py 수정 - 인증 세션 삭제 - 회원가입뷰 작성 - 프로필 detail 뷰 작성 - 프로필 update 뷰 작성 - 프로필 비활성화 뷰 작성 - 계정 삭제 뷰 작성 - 로그인, 로그아웃 뷰 작성 필요 - 이후 인증 구현 필요

--- accounts/views.py
+++ accounts/views.py
@@ -1,26 +1,90 @@
-from rest_framework import generics, permissions, status
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import UserSerializer, MyTokenObtainPairSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
+from .serializers import UserSerializer, ProfileSerializer
 
 User = get_user_model()
 
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = (permissions.AllowAny,)
-    serializer_class = UserSerializer
+class SignUpView(generics.CreateAPIView):
+    """
+    회원가입 API View
+    userserializer 사용
+    """
 
-
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
-
-
-class UserDetailView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = [AllowAny]
+
+
+class ProfileDetailView(generics.RetrieveAPIView):
+    """
+    프로필 API view
+    profileserializer 사용
+    """
+
+    queryset = User.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = "username"
+
+
+class ProfileUpdateView(generics.UpdateAPIView):
+    """
+    프로필 수정 API view
+    """
+
+    queryset = User.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_object(self):
         return self.request.user
+
+
+class UserDeactivateView(APIView):
+    """
+    계정 비활성화 API view
+    user.is_active = False로 계정 로그인 불가
+    이후 계정을 다시 활성화 가능
+    로그인, 로그아웃 뷰 개발 시 비활성화 유저 로그아웃 로직 추가 필요
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        user.is_active = False
+        user.save()
+        return Response(
+            {"detail": "귀하의 계정이 비활성화되었습니다."}, status=status.HTTP_200_OK
+        )
+
+
+class UserDeleteView(APIView):
+    """
+    계정 삭제 API view
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        """
+        confirmation code를 받아서 DELETE request
+        """
+        user = request.user
+        confirmation = request.data.get("confirmation", "")
+
+        if confirmation != "DELETE":
+            return Response(
+                {"detail": "올바른 확인 코드를 입력해주세요."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user.delete()
+        return Response(
+            {"detail": "귀하의 계정이 완전히 삭제되었습니다."},
+            status=status.HTTP_200_OK,
+        )
