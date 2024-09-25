204a0bff06a2828e6d33948845572b212ab2760e
lwj0712 <dldnjswot@naver.com>
2024-09-25 17:00:30 +0900

    🔧 conf: chat>views.py 업데이트
    - 채팅이 삭제 되지 않게 save하는 로직 추가
    - chatroom_list 추가, create_message 추가

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import ChatRoom, Message
 
 
 def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name": room_name})
+    room = get_object_or_404(ChatRoom, name=room_name)
+    messages = Message.objects.filter(room=room).order_by("sent_at")
+    return render(request, "chat/room.html", {"room": room, "messages": messages})
 
 
 def create_room(request):
     if request.method == "POST":
         room_name = request.POST.get("room_name")
-        return redirect("room", room_name=room_name)
+        if room_name:
+            new_room = ChatRoom(name=room_name)
+            new_room.save()
+            return redirect("chatroom_list")
     return render(request, "chat/create_room.html")
+
+
+def chatroom_list(request):
+    chat_rooms = ChatRoom.objects.all()
+    return render(request, "chat/chatroom_list.html", {"chat_rooms": chat_rooms})
+
+
+def create_message(request, room_name):
+    if request.method == "POST":
+        room = get_object_or_404(ChatRoom, name=room_name)
+        content = request.POST.get("message")
+        if content:
+            Message.objects.create(room=room, content=content)
+    return redirect("room", room_name=room_name)
