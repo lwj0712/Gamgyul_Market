e7a67c81af0ec7df2a39a5f890c1c45a5fc7e611
lwj0712 <dldnjswot@naver.com>
2024-09-26 22:51:19 +0900

    🔧 conf: chat>urls.py 업데이트
    - app_name 추가(다른 앱과 중복 방지)
    - API 엔드포인트 url 추가

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,25 @@
 from django.urls import path
 from . import views
 
+app_name = "chat"
+
 urlpatterns = [
-    path("room/<str:room_name>/", views.room, name="room"),
-    path("create/", views.create_room, name="create_room"),
-    path("", views.chatroom_list, name="chatroom_list"),
+    path("room/<str:room_name>/", views.ChatRoomView.as_view(), name="room"),
+    path("create/", views.CreateChatRoomView.as_view(), name="create_room"),
+    path("", views.ChatRoomListView.as_view(), name="chatroom_list"),
+    path(
+        "room/<str:room_name>/leave/", views.LeaveRoomView.as_view(), name="leave_room"
+    ),
+    # API endpoints
+    path("api/user_rooms/", views.GetUserRoomsView.as_view(), name="get_user_rooms"),
+    path(
+        "api/chat_messages/<str:room_name>/",
+        views.GetChatMessagesView.as_view(),
+        name="get_chat_messages",
+    ),
+    path(
+        "api/message/<str:room_name>/",
+        views.CreateMessageView.as_view(),
+        name="create_message",
+    ),
 ]
