94f04889cabcb0313692879e1d311beba04f0666
whaehofk521 <whaehofk521@gmail.com>
2024-09-27 16:38:20 +0900

    🔧 conf: insta/serializers.py tag관련 부분 수정, PostImageSerializer 추가
    - Hashtag, PostHashtag 모델 삭제
    - PostImageSerializer 추가로 여러 개의 이미지를 게시글에 연결
    - PostSerializer에 tags, images 필드 추가로 게시글에 추가 및 포함될 태그, 이미지 처리
    - CommentSerializer, LikeSerializer 필드를 “__all__”로 수정

--- insta/serializers.py
+++ insta/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
-from .models import Post, PostImage, Comment, Hashtag, PostHashtag, Like
+from .models import Post, PostImage, Comment, Like
+
+
+class PostImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImage
+        fields = ["id", "image"]
 
 
 class PostSerializer(serializers.ModelSerializer):
+    tags = serializers.ListField(
+        child=serializers.CharField(max_length=50), required=False
+    )
+    images = PostImageSerializer(many=True, required=False)
+
     class Meta:
         model = Post
         fields = [
@@ -12,34 +23,29 @@ class PostSerializer(serializers.ModelSerializer):
             "location",
             "created_at",
             "updated_at",
+            "tags",
+            "images",
         ]
 
+    def create(self, validated_data):
+        tags_data = validated_data.pop("tags", [])
+        images_data = validated_data.pop("images", [])
+        post = Post.objects.create(**validated_data)
+        post.tags.add(*tags_data)
 
-class PostImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PostImage
-        fields = ["id", "post", "image"]
+        for image_data in images_data:
+            PostImage.objects.create(post=post, **image_data)
+
+        return post
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ["id", "post", "user", "parent_comment", "content", "created_at"]
-
-
-class HashtagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hashtag
-        fields = ["id", "name", "created_at"]
-
-
-class PostHashtagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PostHashtag
-        fields = ["id", "post", "hashtag", "created_at"]
+        fields = "__all__"
 
 
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
-        fields = ["id", "post", "user", "created_at"]
+        fields = "__all__"
