72450d78e9a969fa54da09b12e86e296299b8c05
lwj0712 <dldnjswot@naver.com>
2024-09-29 17:50:30 +0900

    🔧 conf: models.py 수정
    - uuid 도입에 따른 ChatRoom 모델 수정
    - ChatParticipant 모델 불필요하다 판단하여 삭제
    - Message 모델에서 sent_at과 중복되는 created_at 삭제
    - Message 모델에 image 추가, static>chat_images로 임시 저장 위치 설정

--- chat/models.py
+++ chat/models.py
@@ -1,29 +1,34 @@
-from django.conf import settings
+import uuid
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class ChatRoom(models.Model):
-    name = models.CharField(max_length=100)
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )  # UUID로 방 ID 설정
+    participants = models.ManyToManyField(
+        User, related_name="chat_rooms"
+    )  # 사용자와의 다대다 관계 설정
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
-
-
-class ChatParticipant(models.Model):
-    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.user.username} in Room {self.room.id}"
+        return f"{self.participants.first().username}님과의 대화"
 
 
 class Message(models.Model):
-    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.TextField()
-    is_read = models.BooleanField(default=False)
+    chat_room = models.ForeignKey(
+        ChatRoom, related_name="messages", on_delete=models.CASCADE
+    )  # 어느 채팅방의 메시지인지
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(blank=True, null=True)
+    image = models.ImageField(
+        upload_to="static/chat_images/", blank=True, null=True
+    )  # 임시 저장 위치
     sent_at = models.DateTimeField(auto_now_add=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.sender.username}님의 메시지"
