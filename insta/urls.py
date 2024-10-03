8f01eb4bbc78ae8bf2fe4ee94f783b3fdc7b7bd0
whaehofk521 <whaehofk521@gmail.com>
2024-10-03 09:49:09 +0900

    🔧 conf: insta/urls.py urlpattern 추가
    - 게시물 생성, 삭제, 댓글 삭제 url 추가

--- insta/urls.py
+++ insta/urls.py
@@ -1,11 +1,24 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from .views import PostList, PostDetail, CommentList, CommentDetail
+from .views import (
+    PostList,
+    PostDetail,
+    PostCreate,
+    PostDelete,
+    CommentList,
+    CommentDetail,
+    CommentDelete,
+)
 
 app_name = "insta"
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="post-list"),
     path("posts/<int:pk>/", PostDetail.as_view(), name="post-detail"),
+    path("posts/create/", PostCreate.as_view(), name="post-create"),
+    path("posts/<int:pk>/delete/", PostDelete.as_view(), name="post-delete"),
     path("posts/<int:post_id>/comments/", CommentList.as_view(), name="comment-list"),
     path("comments/<int:pk>/", CommentDetail.as_view(), name="comment-detail"),
+    path("comments/<int:pk>/delete", CommentDelete.as_view(), name="comment-delete"),
 ]
