e3c97b4cfefeb38aeeec9d844e261abfbff99149
lwj0712 <dldnjswot@naver.com>
2024-10-03 16:41:31 +0900

    🔧 conf: config>asgi.py에 alarm routing을 결합하여 추가
    - 다양한 앱에서 오는 WebSocket 요청을 하나의 asgi 앱에서 처리할 수 있도록 설계

--- config/asgi.py
+++ config/asgi.py
@@ -9,14 +9,16 @@ django_asgi_app = get_asgi_application()
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from chat.routing import websocket_urlpatterns
-
+from chat.routing import websocket_urlpatterns as chat_websocket_urlpatterns
+from alarm.routing import websocket_urlpatterns as alarm_websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+            AuthMiddlewareStack(
+                URLRouter(chat_websocket_urlpatterns + alarm_websocket_urlpatterns)
+            )
         ),
     }
 )
