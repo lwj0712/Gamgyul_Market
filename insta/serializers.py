c3512f61df60fe93a900c9f42b3017b7ec272c2d
whaehofk521 <whaehofk521@gmail.com>
2024-10-03 09:44:19 +0900

    🔧 conf: insta/serializers.py PostSerializer 수정
    - 게시물이 생성될 때 해당 사용자의 정보가 자동으로 user 필드에 할당되게 수정
    - create 메서드에 이미지 수 제한 추가
    - 게시물이 수정될 때 사용될 update 메서드 추가
    - 태그 정보를 클라이언트에 전달하기 위해 to_representation 메서드 추가

--- insta/serializers.py
+++ insta/serializers.py
@@ -9,16 +9,12 @@ class PostImageSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
-    tags = serializers.ListField(
-        child=serializers.CharField(max_length=50), required=False
-    )
     images = PostImageSerializer(many=True, required=False)
 
     class Meta:
         model = Post
         fields = [
             "id",
-            "user",
             "content",
             "location",
             "created_at",
@@ -26,17 +22,53 @@ class PostSerializer(serializers.ModelSerializer):
             "tags",
             "images",
         ]
+        read_only_fields = ["user", "created_at", "updated_at"]
 
     def create(self, validated_data):
         tags_data = validated_data.pop("tags", [])
         images_data = validated_data.pop("images", [])
+
+        request = self.context.get("request")
+        validated_data["user"] = request.user
+
         post = Post.objects.create(**validated_data)
-        post.tags.add(*tags_data)
+
+        if len(images_data) + post.images.count() > 10:
+            raise serializers.ValidationError("이미지는 10개까지 첨부할 수 있습니다.")
 
         for image_data in images_data:
             PostImage.objects.create(post=post, **image_data)
 
+        post.tags.add(*tags_data)
+
         return post
+
+    def update(self, instance, validated_data):
+        tags_data = validated_data.pop("tags", [])
+        images_data = validated_data.pop("images", [])
+
+        instance.content = validated_data.get("content", instance.content)
+        instance.location = validated_data.get("location", instance.location)
+        instance.save()
+
+        if tags_data:
+            instance.tags.set(tags_data)
+
+        if images_data:
+            if len(images_data) + instance.images.count() > 10:
+                raise serializers.ValidationError(
+                    "이미지는 10개까지 첨부할 수 있습니다."
+                )
+
+            for image_data in images_data:
+                PostImage.objects.create(post=instance, **image_data)
+
+        return instance
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["tags"] = [str(tag) for tag in instance.tags.all()]
+        return representation
 
 
 class CommentSerializer(serializers.ModelSerializer):
