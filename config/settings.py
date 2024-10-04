3f9b613e526316763359b51a2fd01342b53f155e
lwj0712 <dldnjswot@naver.com>
2024-10-04 16:05:57 +0900

    🔧 conf: settings.py 수정
    - S3 설정 이전으로 되돌림

--- config/settings.py
+++ config/settings.py
@@ -142,35 +142,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-# S3 사용 설정
-USE_S3 = os.getenv("USE_S3") == "TRUE"
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
-# 공통 설정
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
-if USE_S3:
-    # AWS Setting
-    AWS_REGION = os.getenv("AWS_REGION")
-    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com"
-    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-
-    # s3 static settings
-    STATIC_LOCATION = "static"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
-
-    # s3 media settings
-    PUBLIC_MEDIA_LOCATION = "media"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
-    DEFAULT_FILE_STORAGE = "final_project.storage_backends.PublicMediaStorage"
-else:
-    STATIC_URL = "/static/"
-    MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
