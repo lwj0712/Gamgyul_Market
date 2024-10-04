030a24f9e165b47355bb71bd7a7e5df01d1c0185
Donggyu-Kim <kimdg000411@gmail.com>
2024-10-04 17:29:56 +0900

    :test_tube: test: recommendation 테스트 코드 작성 - 팔로워 기반 추천 테스트 - 태그 기반 추천 테스트 - 거래 기반 추천 테스트 - 인기 사용자 추천 테스트 - 최대 15명까지만 추천 테스트 - 인증된 사용자 테스트

--- alarm/urls.py
+++ alarm/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = "alarm"
 
 urlpatterns = [
-    # path("", views.AlarmListView.as_view(), name="alarm_list"),
-    # path(
-    #     "<uuid:alarm_id>/read/",
-    #     views.AlarmReadView.as_view(),
-    #     name="alarm_read",
-    # ),  # 특정 알림을 읽음 상태로 바꾸는 url
+    path("", views.AlarmListView.as_view(), name="alarm_list"),
+    path(
+        "<uuid:alarm_id>/read/",
+        views.AlarmReadView.as_view(),
+        name="alarm_read",
+    ),  # 특정 알림을 읽음 상태로 바꾸는 url
 ]
