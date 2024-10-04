fb382ac9e7bae22bea0e42847a24b302f5379ddb
Donggyu-Kim <kimdg000411@gmail.com>
2024-10-05 00:03:32 +0900

    :card_file_box: db: 위도 경도 필드 삭제

--- accounts/models.py
+++ accounts/models.py
@@ -30,8 +30,6 @@ class User(AbstractUser):
     )
     nickname = models.CharField(max_length=50, unique=True)
     bio = models.TextField(max_length=500, blank=True)
-    latitude = models.FloatField(null=True, blank=True)
-    longitude = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.username
