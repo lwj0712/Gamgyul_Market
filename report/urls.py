9f6e5c33aed5bb58a90b4e65022c6cdc98b81b14
Donggyu-Kim <kimdg000411@gmail.com>
2024-10-07 12:26:59 +0900

    :sparkles: feat: urls.py, views.py 작성 - createview로 신고 생성 뷰 작성 - 인증된 사용자만 가능 - url 생성

--- report/urls.py
+++ report/urls.py
@@ -1,22 +1,8 @@
-from rest_framework import serializers
-from .models import Report
-from django.contrib.contenttypes.models import ContentType
+from django.urls import path
+from . import views
 
+app_name = "report"
 
-class ReportCreateSerializer(serializers.ModelSerializer):
-    content_type = serializers.CharField()
-    object_id = serializers.IntegerField()
-
-    class Meta:
-        model = Report
-        fields = ["content_type", "object_id", "reason", "description"]
-
-    def validate_content_type(self, value):
-        try:
-            return ContentType.objects.get(model=value)
-        except ContentType.DoesNotExist:
-            raise serializers.ValidationError("Invalid content type")
-
-    def create(self, validated_data):
-        validated_data["reporter"] = self.context["request"].user
-        return super().create(validated_data)
+urlpatterns = [
+    path("create/", views.ReportCreateView.as_view(), name="report-create"),
+]
--- report/views.py
+++ report/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Report
+from .serializers import ReportCreateSerializer
 
-# Create your views here.
+
+class ReportCreateView(generics.CreateAPIView):
+    """신고 생성 뷰"""
+
+    queryset = Report.objects.all()
+    serializer_class = ReportCreateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(reporter=self.request.user)
