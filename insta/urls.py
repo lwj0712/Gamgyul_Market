2f1b666b40c65b1419b2a71b7f79a590b7006e38
whaehofk521 <whaehofk521@gmail.com>
2024-10-07 10:32:15 +0900

    🔧 conf: insta/urls.py 수정
    - 코드컨벤션에 맞게 수정
    - 게시물 좋아요 url 추가

--- insta/urls.py
+++ insta/urls.py
@@ -2,23 +2,29 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from .views import (
-    PostList,
-    PostDetail,
-    PostCreate,
-    PostDelete,
-    CommentList,
-    CommentDetail,
-    CommentDelete,
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostDeleteView,
+    CommentListView,
+    CommentDetailView,
+    LikeView,
 )
 
 app_name = "insta"
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="post-list"),
-    path("posts/<int:pk>/", PostDetail.as_view(), name="post-detail"),
-    path("posts/create/", PostCreate.as_view(), name="post-create"),
-    path("posts/<int:pk>/delete/", PostDelete.as_view(), name="post-delete"),
-    path("posts/<int:post_id>/comments/", CommentList.as_view(), name="comment-list"),
-    path("comments/<int:pk>/", CommentDetail.as_view(), name="comment-detail"),
-    path("comments/<int:pk>/delete", CommentDelete.as_view(), name="comment-delete"),
-]
+    path("posts/", PostListView.as_view(), name="insta_post_list"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="insta_post_detail"),
+    path("posts/create/", PostCreateView.as_view(), name="insta_post_create"),
+    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="insta_post_delete"),
+    path(
+        "posts/<int:post_id>/comments/",
+        CommentListView.as_view(),
+        name="insta_comment_list",
+    ),
+    path(
+        "comments/<int:pk>/", CommentDetailView.as_view(), name="insta_comment_detail"
+    ),
+    path("posts/<int:post_id>/like/", LikeView.as_view(), name="insta_like"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
