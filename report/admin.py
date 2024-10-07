4669565bb73c82100aededdf4790cf12824bd63f
Donggyu-Kim <kimdg000411@gmail.com>
2024-10-07 12:40:16 +0900

    :sparkles: feat: admin 페이지 생성 - 앱 내 모든 필드를 대상으로 신고 관리 기능 구현

--- report/admin.py
+++ report/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Report
 
-# Register your models here.
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    """
+    신고된 필드는 admin이 수정 불가
+    status, comment만 가능
+    """
+
+    list_display = (
+        "id",
+        "reporter",
+        "content_type",
+        "object_id",
+        "reason",
+        "status",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("status", "reason", "content_type")
+    search_fields = ("reporter__username", "description", "admin_comment")
+    readonly_fields = (
+        "reporter",
+        "content_type",
+        "object_id",
+        "reason",
+        "description",
+        "created_at",
+    )
+    fields = (
+        "reporter",
+        "content_type",
+        "object_id",
+        "reason",
+        "description",
+        "status",
+        "admin_comment",
+        "created_at",
+        "updated_at",
+    )
+
+    def has_add_permission(self, request):
+        return False
