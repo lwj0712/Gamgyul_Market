a41bbcc58514a0080e7fb00645ad7ba20d790386
lwz7 <dldnjswot@naver.com>
2024-10-12 02:14:05 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 if not SECRET_KEY:
     raise ValueError("The SECRET_KEY setting must not be empty.")
