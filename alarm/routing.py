a651ad4492996db94e516481bca81a9894ad13fb
Donggyu-Kim <kimdg000411@gmail.com>
2024-10-12 19:09:38 +0900

    routing url 수정

--- alarm/routing.py
+++ alarm/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/alarm/$", consumers.AlarmConsumer.as_asgi()),
+    re_path(r"ws/alarm/(?P<user_id>\w+)/$", consumers.AlarmConsumer.as_asgi()),
 ]
