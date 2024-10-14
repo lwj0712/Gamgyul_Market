eee34a296797d7fec0d4b6d4bbf3b4119ae4a69b
Donggyu-Kim <kimdg000411@gmail.com>
2024-10-14 22:52:54 +0900

    drf 인증 순서 변경

--- config/settings.py
+++ config/settings.py
@@ -167,8 +167,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Django Rest Framework 설정
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
